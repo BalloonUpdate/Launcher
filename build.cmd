dd12d1e82e283331494c545abc4483d1f6247441
innc11 <pm@innc11.cn>
2021-02-06 21:12:48 +0800

    a

--- build.cmd
+++ build.cmd
@@ -1,10 +1,23 @@
 @echo off
 
-python version.py > temp.txt
+python ci\generate_version_file.py
+
+python ci\version.py both > temp.txt
 set /p filename=< temp.txt
+
+python ci\version.py version > temp.txt
+set /p version_text=< temp.txt
+
 del temp.txt
 
-echo Build for %filename%
+del /f /s /q dist\%filename%.exe
 
+echo ----------------Build for %filename%----------------
 pyinstaller --noconfirm --version-file version-file.txt -i icon.ico -c -F -n %filename% LauncherMain.py %1
-echo Build finished!
+
+echo ----------------Clean up----------------
+
+del /f /s /q version-file.txt
+del /f /s /q %filename%.spec
+
+echo ----------------Build %filename% finished!----------------
